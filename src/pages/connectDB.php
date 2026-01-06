e4d12144929cae11f68b27cf0f4977b218ff8eb8
Wafaa Obaid <wafaawessam2006@gmail.com>
2026-01-06 13:34:48 +0200

    docker: fix docker-compose services and use env vars for database connection

--- src/pages/connectDB.php
+++ src/pages/connectDB.php
@@ -1,11 +1,11 @@
 <?php
 
-$connection = new mysqli(
-    "db",
-    "root",
-    "12345",
-    "masterstouchstore"
-);
+$host = getenv("DB_HOST") ?: "localhost";
+$user = getenv("DB_USER") ?: "root";
+$pass = getenv("DB_PASS") ?: "12345";
+$db = getenv("DB_NAME") ?: "masterstouchstore";
+
+$connection = new mysqli($host, $user, $pass, $db);
 
 if ($connection->connect_error) {
     die("Database connection failed");
