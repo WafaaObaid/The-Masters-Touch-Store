bad2f35ec77a169585f05a5ee1126758d8428bff
Wafaa Obaid <wafaawessam2006@gmail.com>
2026-01-08 16:20:19 +0200

    Update Project

--- src/pages/connectDB.php
+++ src/pages/connectDB.php
@@ -1,11 +1,11 @@
 <?php
 
-$connection = new mysqli(
-    "db",
-    "root",
-    "12345",
-    "masterstouchstore"
-);
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db = getenv('DB_NAME');
+
+$connection = new mysqli($host, $user, $pass, $db);
 
 if ($connection->connect_error) {
     die("Database connection failed");
